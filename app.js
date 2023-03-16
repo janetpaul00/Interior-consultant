75a154d786a30db1e6199e140a6fa1f28a9525a7
janetpaul00 <84874007+janetpaul00@users.noreply.github.com>
2023-03-16 15:55:18 +0400

    updates

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const button = document.querySelector("#button");
-
 function menu() {
-  console.log("button is clicked");
-  window.location.href = "nav-page.html";
+  console.log("menu is clicked");
+
+  const nav = document.querySelector(".navbar");
+
+  if (nav.className.includes("open")) {
+    nav.classList.remove("open");
+  } else {
+    nav.classList.add("open");
+  }
 }
